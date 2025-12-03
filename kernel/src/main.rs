cd6a41e453f3e9973cd185c529b5eeee91cb3fc0
Samhith Enganti <enganti.samhith@gmail.com>
2025-12-03 00:18:08 -0500

    Refactor kernel_main to improve framebuffer handling and remove unused imports

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,6 +3,7 @@
 
 use bootloader_api::{BootInfo, entry_point};
 use core::fmt::Write;
+use x86_64::instructions::{nop, port::Port, hlt};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
@@ -12,8 +13,6 @@ pub enum QemuExitCode {
 }
 
 pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
-    use x86_64::instructions::{nop, port::Port};
-
     unsafe {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
@@ -39,8 +38,25 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     writeln!(port, "Entered kernel with boot info: {boot_info:?}").unwrap();
     writeln!(port, "\nhello, world\n").unwrap();
 
+    // NOTE: The VGA text buffer at 0xb8000 is not available!
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        let info = framebuffer.info();
+        let buffer = framebuffer.buffer_mut();
 
-    exit_qemu(QemuExitCode::Success);
+        // 2. Iterate over every pixel on the screen
+        // The buffer is just a long slice of u8 bytes.
+        // We jump by 'bytes_per_pixel' (which is 3) to get to the next pixel.
+        for pixel in buffer.chunks_exact_mut(info.bytes_per_pixel) {
+            // 3. Write the color. Format is BGR (Blue, Green, Red) based on your logs.
+            pixel[0] = 255; // Blue
+            pixel[1] = 0; // Green
+            pixel[2] = 0; // Red
+        }
+    }
+
+    loop {
+        hlt();
+    }
 }
 
 /// This function is called on panic.
