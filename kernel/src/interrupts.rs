7ce07470de8c181c1fcaad7cd65e25eabf1659ec
Samhith E <enganti.samhith@gmail.com>
2025-12-04 12:11:47 -0500

    added IDT

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -1,13 +1,18 @@
-use crate::serial::println_serial;
+use crate::serial_println;
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
-        // Link the specific exception to our handler function
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler);
+
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(crate::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
         idt
     };
 }
@@ -19,7 +24,7 @@ pub fn init_idt() {
 // INTERRUPT HANDLERS
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
-    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+    serial_println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
 extern "x86-interrupt" fn double_fault_handler(
